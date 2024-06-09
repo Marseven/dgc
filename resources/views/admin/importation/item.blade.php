386e60ddfca9e49fc66fbd053b924f0e84128e4e
Aristide Mebodo <mebodoaristide@yahoo.fr>
2024-06-09 17:00:58 +0100

    Debug Admin 3

--- resources/views/admin/importation/item.blade.php
+++ resources/views/admin/importation/item.blade.php
@@ -9,7 +9,7 @@
         <div class="page-title">
             <div class="row">
                 <div class="col-6">
-                    <h4>Déclaration d'importation</h4>
+                    <h4>Déclaration d'importation et d'exportation</h4>
                 </div>
                 <div class="col-6">
                     <ol class="breadcrumb">
