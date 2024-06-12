be0dbe74abab9b1056054a87783d564bc61690dc
Aristide Mebodo <mebodoaristide@yahoo.fr>
2024-06-12 12:22:40 +0100

    Add Statut 15

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,9 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            @if (trim($slot) === 'Laravel')
-                <img src="{{ asset('front/images/dgc.png') }}" class="logo" alt="Logo DGC">
-            @else
-                {{ $slot }}
-            @endif
+
+            <img src="{{ asset('front/images/dgc_wb.png') }}" class="logo" alt="Logo DGC">
+
         </a>
     </td>
 </tr>
