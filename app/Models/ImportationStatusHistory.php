2741e27d650427e84acb279136e50fc92b8b3cf6
Aristide Mebodo <mebodoaristide@yahoo.fr>
2024-06-16 18:59:37 +0100

    Add Statut 31

--- app/Models/ImportationStatusHistory.php
+++ app/Models/ImportationStatusHistory.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class ImportationStatusHistory extends Model
 {
     use HasFactory;
+
+    protected $table = 'importation_status_history';
+
+    protected $fillable = [
+        'old_status',
+        'new_status',
+        'message',
+        'user_id',
+        'importation_id',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'user_id',
+        'importation_id',
+    ];
 }
--- app/Models/StockStatusHistory.php
+++ app/Models/StockStatusHistory.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class StockStatusHistory extends Model
 {
     use HasFactory;
+
+    protected $table = 'stock_status_history';
+
+    protected $fillable = [
+        'old_status',
+        'new_status',
+        'message',
+        'user_id',
+        'stock_id',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'user_id',
+        'stock_id',
+    ];
 }
